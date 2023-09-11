dda8256252ad3809ac811933cff88516161ba729
doddapranit <doddapranit@gmail.com>
2023-09-11 01:57:23 -0400

    bare bones

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithubAlt} from '@fortawesome/free-brands-svg-icons';
+import {faFile} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="centered-container">
+        <h1 className="centered-text">Pranit Dodda</h1>
+        <div className="work-in-progress">Work in Progress...</div>
+        <div className="icons">
+          <a href="https://www.linkedin.com/in/pranit-dodda-97344b204/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} size="3x" />
+          </a>
+          <a href="https://drive.google.com/file/d/1d9cMe4xIs6fkUf_PeoLCD1BYstYLtGLo/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <h1>Resume</h1>
+          </a>
+          <a href="https://github.com/doddapranit/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithubAlt} size="3x" />
+          </a>
+      </div>
+      </div>
     </div>
   );
 }
